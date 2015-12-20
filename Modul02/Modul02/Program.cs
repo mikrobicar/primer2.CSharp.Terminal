18e107476ad9e874d6f0414424b28a8dbc217ff7
Борис Симеонов <bsimeonov@abv.bg>
2015-12-21 00:13:50 +0200

    Модул 02, Видео 03 - Основни оператори за целочислен тин данни

--- Modul02/Modul02/Program.cs
+++ Modul02/Modul02/Program.cs
@@ -11,8 +11,8 @@ namespace Modul02
 			int sum = 0;
 
 			//Събиране на числа
-			a = 75;
-			b = 10;
+			a = 10;
+			b = 20;
 
 			sum = a + b;
 
@@ -22,6 +22,56 @@ namespace Modul02
 			Console.Write ( b );
 			Console.Write (" = ");
 			Console.WriteLine ( sum );
+
+			//Допълнителни оператори
+			Console.WriteLine ("\nИзползване на +=");
+			a += b;
+			Console.WriteLine (  a );
+
+			Console.WriteLine ("\nИзползване на -=");
+			a -= b;
+			Console.WriteLine (  a );
+			
+			Console.WriteLine ("\nИзползване на ++");
+			a ++;
+			Console.WriteLine (  a );
+
+			Console.WriteLine ("\nИзползване на --");
+			a --;
+			Console.WriteLine (  a );
+
+			Console.WriteLine ("\nИзползване на *=");
+			a *= b;
+			Console.WriteLine (  a );
+			Console.ReadKey ();
+
+			Console.WriteLine ("\n");
+			Console.Write ("Пример:7/3");
+			Console.WriteLine ("\n");
+			Console.WriteLine ("Стандартно делене: " + (7 / 3).ToString ()); // ->2 (7/3=2,333)
+			Console.WriteLine ("Остатък от делене: " + (7 % 3).ToString ()); // ->1 (1/3=0,333)
+			Console.ReadKey ();
+
+			Console.WriteLine ("\n");
+			Console.Write ("Пример-7/4");
+			Console.WriteLine ("\n");
+			Console.WriteLine ("Стандартно делене: " + (7 / 4).ToString ()); // ->1 (7/4=1,75)
+			Console.WriteLine ("Остатък от делене: " + (7 % 4).ToString ()); // ->3 (3/4=0,75)
+			Console.ReadKey ();
+
+			Console.WriteLine ("\n");
+			Console.Write ("Пример-14/4");
+			Console.WriteLine ("\n");
+			Console.WriteLine ("Стандартно делене: " + (14 / 4).ToString ()); // ->3 (14/4=3.5)
+			Console.WriteLine ("Остатък от делене: " + (14 % 4).ToString ()); // ->2 (2/4=0,5)
+			Console.ReadKey ();
+
+			Console.WriteLine ("\n");
+			Console.Write ("Пример->15/3");
+			Console.WriteLine ("\n");
+			Console.WriteLine ("Стандартно делене: " + (15 / 3).ToString ()); // ->5 (15/3=5)
+			Console.WriteLine ("Остатък от делене: " + (15 % 3).ToString ()); // ->0 (0/0=0)
+			Console.ReadKey ();
 		}
 	}
 }
