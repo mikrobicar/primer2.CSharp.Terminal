45886992b2c92dc8e215c9b0a9ca767aa5ad6be0
Борис Симеонов <bsimeonov@abv.bg>
2016-01-13 10:19:05 +0200

    Модул 03, Видео 02 - форматиране на дробни числа

--- Modul02/TableProject/Program.cs
+++ Modul02/TableProject/Program.cs
@@ -6,20 +6,46 @@ namespace TableProject
 	{
 		public static void Main (string[] args)
 		{
+//
+//			//Дефиниране на таблица
+//			string[,] table = new string[2, 3];
+//
+//			//Въвеждане на първи ред
+//			table[0, 0] = "Борис"; table[0, 1] = "Симеонов"; table[0, 2] = "46";
+//
+//			//Въвеждане на втори ред
+//			table[1, 0] = "Борис"; table[1, 1] = "Симеонов"; table[1, 2] = "47";
+//
+//			//Печат
+//			Console.Write ("Кой ред искате да видите: ");
+//			int _index = Convert.ToInt32 (Console.ReadLine ()) - 1;
+//			Console.WriteLine ((table [_index, 0]) [0] + ". " + table [_index, 1] + ", " + table [_index, 2] + " г.");
+//
+//
+//			//Шах
+//			string[,] shah = new String[3,3];
+//			shah[1,1] = "Х";
+//			Console.WriteLine (shah [1,1]);
 
-			//Дефиниране на таблица
-			string[,] table = new string[2, 3];
+//			int[,] a = new int [3,3]
+//			{{1,2,3}, {4,5,6}, {7,8,9}};
+//			Console.WriteLine(a[0,0]+" "+a[0,1]+" "+a[0,2]);
+//			Console.WriteLine(a[1,0]+" "+a[1,1]+" "+a[1,2]);
+//			Console.WriteLine(a[2,0]+" "+a[2,1]+" "+a[2,2]);
 
-			//Въвеждане на първи ред
-			table[0, 0] = "Борис"; table[0, 1] = "Симеонов"; table[0, 2] = "46";
+			string[] row = new string[3];
+			row [0] = "Ред 1";
+			row [1] = "Ред 2";
+			row [2] = "Ред 3";
 
-			//Въвеждане на втори ред
-			table[1, 0] = "Борис"; table[1, 1] = "Симеонов"; table[1, 2] = "47";
+			Console.WriteLine ("Масив row:" + row [0] + ", " + row [1] + ", " + row [2] + "\n");
 
-			//Печат
-			Console.Write ("Кой ред искате да видите: ");
-			int _index = Convert.ToInt32 (Console.ReadLine ()) - 1;
-			Console.WriteLine ((table [_index, 0]) [0] + ". " + table [_index, 1] + ", " + table [_index, 2] + " г.");
+			string[] parse = "1,2,3,4,5,6,7,8,9".Split (',');
+			Console.WriteLine ("Броят на цифрите в стринга:\n" + "1,2,3,4,5,6,7,8,9\n\ne:" + parse.Length);
+
+			string list1 = string.Join (";", parse);
+			Console.WriteLine ("Новиат стринг е:\n" + list1 + "\n\n");
+
 
 
 		}
@@ -28,15 +54,15 @@ namespace TableProject
 
 
 
-//string[] row = new string[3];
-//row [0] = "Ред 1";
-//row [1] = "Ред 2";
-//row [2] = "Ред 3";
-//
-//Console.WriteLine ("Масив row:" + row [0] + ", " + row [1] + ", " + row [2] + "\n");
-//
-//string[] parse = "1,2,3,4,5,6,7,8,9".Split (',');
-//Console.WriteLine ("Броят на цифрите в стринга:\n" + "1,2,3,4,5,6,7,8,9\n\ne:" + parse.Length);
-//
-//string list1 = string.Join (";", parse);
-//Console.WriteLine ("Новиат стринг е:\n" + list1 + "\n\n");
+
+
+	
+
+
+
+
+
+
+
+
+
---
+++ Modul03/Modul03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Modul03
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+			double a = 0, b = 0, c = 0;
+			Console.Write ("> a = "); a = Convert.ToDouble(Console.ReadLine () );
+			Console.Write ("> b = "); b = Convert.ToDouble(Console.ReadLine () );
+//			
+			string _format = "#0.00 м3";
+
+			Console.WriteLine ("\n\n>a / b = " + ( a / b).ToString(_format));
+		}
+	}
+}
+//double a=0, b=0;
+//a = 1.345;
+//string d = "1.687"; b = Convert.ToDouble(d);
+//Console.WriteLine( a + b );
